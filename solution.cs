a67c2a05fbc308e0425eda42e17c3eb61fb4c353
Kooreis <kooreis.official@gmail.com>
2024-01-11 22:32:12 -0600

    This update populates the 'graph' Map with the prerequisites. If a course already exists in the graph, it adds the prerequisite to its list. If not, it creates a new entry in the graph.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,7 @@
-function canFinish(numCourses, prerequisites) {
-    let graph = new Map();
-    let visited = new Array(numCourses).fill(0);
-}
+for(let i = 0; i < prerequisites.length; i++) {
+        if(graph.has(prerequisites[i][1])) {
+            graph.get(prerequisites[i][1]).push(prerequisites[i][0]);
+        } else {
+            graph.set(prerequisites[i][1], [prerequisites[i][0]]);
+        }
+    }
